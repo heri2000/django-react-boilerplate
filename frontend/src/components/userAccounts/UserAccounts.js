d4d2925de67602214263074f8c73e4dfea5dd75e
heri2000 <heri2000@live.com>
2021-08-09 22:47:26 +0700

    Change UserAccounts from class to arrow function

--- frontend/src/components/userAccounts/UserAccounts.js
+++ frontend/src/components/userAccounts/UserAccounts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
@@ -16,69 +16,63 @@ import UserEditor from "./UserEditor";
 
 import './UserAccounts.css';
 
-class UserAccounts extends React.Component {
-  constructor(props) {
-    super(props);
+const UserAccounts = (props) => {
+  const [state, setState] = useState({
+    filter: "",
+    buttonAndDataVisibility: "hidden",
+    editUser: false,
+  });
 
-    this.state = {
-      filter: "",
-      buttonAndDataVisibility: "hidden",
-      editUser: false,
-    }
+  const translation = H_GetTranslation();
+  document.title = getTabTitle(translation.user.moduleTitle);
+  
+  const { users } = props.users;
+
+  const columns = [
+    { field: 'username', headerName: translation.user.username, minWidth: 150, flex: 2, editable: false },
+    { field: 'email', headerName: translation.user.email, minWidth: 200, flex: 3, editable: false },
+    { field: 'first_name', headerName: translation.user.first_name, minWidth: 200, flex: 3, editable: false },
+    { field: 'last_name', headerName: translation.user.last_name, minWidth: 200, flex: 3, editable: false },
+    { field: 'is_staff', headerName: translation.user.is_staff, type: "boolean", minWidth: 150, flex: 0, editable: false },
+    { field: 'is_superuser', headerName: translation.user.is_superuser, type: "boolean", minWidth: 150, flex: 0, editable: false },
+    { field: 'is_active', headerName: translation.user.is_active, type: "boolean", minWidth: 150, flex: 0, editable: false },
+  ];
+  
+  const handleShowButtonClick = (filter) => {
+    setState({ ...state, filter: filter, buttonAndDataVisibility: "visible" });
+    props.getUsers(filter);
   }
 
-  render() {
-    const translation = H_GetTranslation();
-    document.title = getTabTitle(translation.user.moduleTitle);
-    
-    const { users } = this.props.users;
+  const handleNewUserButtonClick = () => {
+    setState({ ...state, editUser: true });
+  }
 
-    const columns = [
-      { field: 'username', headerName: translation.user.username, minWidth: 150, flex: 2, editable: false },
-      { field: 'email', headerName: translation.user.email, minWidth: 200, flex: 3, editable: false },
-      { field: 'first_name', headerName: translation.user.first_name, minWidth: 200, flex: 3, editable: false },
-      { field: 'last_name', headerName: translation.user.last_name, minWidth: 200, flex: 3, editable: false },
-      { field: 'is_staff', headerName: translation.user.is_staff, type: "boolean", minWidth: 150, flex: 0, editable: false },
-      { field: 'is_superuser', headerName: translation.user.is_superuser, type: "boolean", minWidth: 150, flex: 0, editable: false },
-      { field: 'is_active', headerName: translation.user.is_active, type: "boolean", minWidth: 150, flex: 0, editable: false },
-    ];
-    
-    const handleShowButtonClick = (filter) => {
-      this.setState({ filter: filter, buttonAndDataVisibility: "visible" });
-      this.props.getUsers(filter);
-    }
+  const handleCloseUserEditor = () => {
+    setState({ ...state, editUser: false });
+  }
 
-    const handleNewUserButtonClick = () => {
-      this.setState({ editUser: true });
-    }
-
-    const handleCloseUserEditor = () => {
-      this.setState({ editUser: false });
-    }
-
-    return( 
-      <AppContainer title={translation.user.moduleTitle}>
-        <div className="PageContent">
-          <UserFilter handleShowButtonClick={handleShowButtonClick} />
-          <div className="ButtonPanel" style={{visibility: this.state.buttonAndDataVisibility}}>
-            <CommonButton onClick={handleNewUserButtonClick}>{translation.user.newUser}</CommonButton>
-            <CommonButton>{translation.user.edit}</CommonButton>
-            <CommonButton>{translation.user.withSelected}</CommonButton>
-          </div>
-          <div className="DataPanel" style={{visibility: this.state.buttonAndDataVisibility}}>
-            <CommonDataGrid columns={columns} rows={users} />
-          </div>
-
-          <UserEditor
-            user={{}}
-            open={this.state.editUser}
-            onClose={handleCloseUserEditor}
-          />
-
+  return( 
+    <AppContainer title={translation.user.moduleTitle}>
+      <div className="PageContent">
+        <UserFilter handleShowButtonClick={handleShowButtonClick} />
+        <div className="ButtonPanel" style={{visibility: state.buttonAndDataVisibility}}>
+          <CommonButton onClick={handleNewUserButtonClick}>{translation.user.newUser}</CommonButton>
+          <CommonButton>{translation.user.edit}</CommonButton>
+          <CommonButton>{translation.user.withSelected}</CommonButton>
         </div>
-      </AppContainer>
-    );
-  }
+        <div className="DataPanel" style={{visibility: state.buttonAndDataVisibility}}>
+          <CommonDataGrid columns={columns} rows={users} />
+        </div>
+
+        <UserEditor
+          user={{}}
+          open={state.editUser}
+          onClose={handleCloseUserEditor}
+        />
+
+      </div>
+    </AppContainer>
+  );
 }
 
 UserAccounts.propTypes = {
